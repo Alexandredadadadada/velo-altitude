90ddc99d03ef10b3e9f7332dc2fe4283a22f29bb
Alexandre <alexandre@example.com>
2025-04-22 15:29:27 +0700

    fix(auth import): correct import path in apiWrapper.js to src/auth

--- client/src/services/apiWrapper.js
+++ client/src/services/apiWrapper.js
@@ -8,7 +8,7 @@
 import axios from 'axios';
 import config from '../config';
 import { handleApiError } from '../utils/apiErrorUtils';
-import auth from '../../auth';
+import auth from '../auth';
 
 /**
  * Wrapper pour les appels API
